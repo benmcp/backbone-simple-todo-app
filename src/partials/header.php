a77db0853fc8b544cc9ed1c034177bb27e36eb69
ben mcphail <ben@benmcphail.com>
2016-02-13 15:56:34 +1100

    Update header

--- src/partials/header.php
+++ src/partials/header.php
@@ -3,10 +3,10 @@
 <!--[if IE 7]>         <html class="lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html> <!--<![endif]-->
-	<head>
+	<head data-framework="backbonejs">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, maximum-scale=1">
-		<title></title>
+		<title>Backbone - Simple Application</title>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	</head>
 	<body >
